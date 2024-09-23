f8e08ef60bd588bc802d8489d374e68c1b9cbdc9
Kimball Bradford <kimball.bradford@gmail.com>
2024-09-24 00:54:56 +0200

    added structured response

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,14 +5,24 @@ import { useTheme } from "utils/useTheme";
 import styles from "./Home.module.sass";
 import axios from "axios";
 import { startTransition } from "preact/compat";
-import { callLLM } from "utils/llmAccess";
+import { callLLM, callLLMStructured, createLLMResponseFormat } from "utils/llmAccess";
+
+const responseStructure = createLLMResponseFormat(
+  "exampleResponse",
+  {
+    happy: {type: "boolean"},
+    response: {type: "string"},
+    notRequired: {type: "number"},
+  },
+  ["happy", "response"],
+)
 
 export const Home: FunctionComponent = () => {
   const { setTheme, theme } = useTheme();
   const [buttonText, setButtonText] = useState("Send Request");
   const sendRequest = async (): Promise<void> => {
     setButtonText('Sending...');
-    const response = await callLLM([{ content: "hello", role: "user" }])
+    const response = await callLLMStructured([{ content: "please put true into happy and some random response into response, and dont include notRequired", role: "user" }], responseStructure)
     setButtonText("OK!");
     console.log(response);
   }
--- src/utils/llmAccess.ts
+++ src/utils/llmAccess.ts
@@ -1,47 +1,94 @@
-
-
-type LLMMessage = { role: "system" | "user" | "assistant", content: string };
+type LLMMessage = { role: "system" | "user" | "assistant"; content: string };
 
 interface LLMRequest {
-  model: string;
-  messages: LLMMessage[];
-  temperature: number;
-  max_tokens: number;
-  stream: boolean;
-  response_format: {
-    type: "json_schema";
-    json_schema: {
-      name: string,
-      strict: "true",
-      schema: {
-        type: "object",
-        properties: {
-          [key: string]: "string" | "number" | "boolean"
-        },
-        required: string[];
-      }
-    }
-  },
+	model: string;
+	messages: LLMMessage[];
+	temperature: number;
+	max_tokens: number;
+	stream: boolean;
+	response_format?: LLMResponseFormat;
 }
 
-export const callLLM = async (messages: LLMMessage[], options?: {
-  model?: string,
-  temperature?: number,
-  max_tokens?: number,
-  stream?: boolean,
-}): Promise<LLMMessage[]> => {
-  return await fetch("http://127.0.0.1:1234/v1/chat/completions", {
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    method: "post",
-    body: JSON.stringify({
-      model: options?.model || "lmstudio-community/Llama-3-Groq-8B-Tool-Use-GGUF/Llama-3-Groq-8B-Tool-Use-Q5_K_M.gguf",
-      messages: messages,
-      temperature: options?.temperature || 0.7,
-      max_tokens: options?.max_tokens || -1,
-      stream: options?.stream || false,
-    })
-  }).then(async r => (await r.json()).choices[0])
-}
+type LLMResponseFormat = {
+	type: "json_schema";
+	json_schema: {
+		name: string;
+		strict: "true";
+		schema: {
+			type: "object";
+			properties: {
+				[key: string]: { type: "string" | "number" | "boolean" };
+			};
+			required: string[];
+		};
+	};
+};
+
+export const createLLMResponseFormat = (
+	name: string,
+	properties: { [key: string]: { type: "string" | "number" | "boolean" } },
+	required: string[]
+): LLMResponseFormat => ({
+	type: "json_schema",
+	json_schema: {
+		name,
+		strict: "true",
+		schema: { properties, type: "object", required },
+	},
+});
+
+export const callLLM = async (
+	messages: LLMMessage[],
+	options?: {
+		model?: string;
+		temperature?: number;
+		max_tokens?: number;
+		stream?: boolean;
+	}
+): Promise<LLMMessage[]> => {
+	return await fetch("http://127.0.0.1:1234/v1/chat/completions", {
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+		method: "post",
+		body: JSON.stringify({
+			model:
+				options?.model ||
+				"lmstudio-community/Llama-3-Groq-8B-Tool-Use-GGUF/Llama-3-Groq-8B-Tool-Use-Q5_K_M.gguf",
+			messages: messages,
+			temperature: options?.temperature || 0.7,
+			max_tokens: options?.max_tokens || -1,
+			stream: options?.stream || false,
+		}),
+	}).then(async (r) => (await r.json()).choices[0]);
+};
+
+export const callLLMStructured = async (
+	messages: LLMMessage[],
+	format: LLMResponseFormat,
+	options?: {
+		model?: string;
+		temperature?: number;
+		max_tokens?: number;
+		stream?: boolean;
+	}
+): Promise<any> => {
+	return await fetch("http://127.0.0.1:1234/v1/chat/completions", {
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+		method: "post",
+		body: JSON.stringify({
+			model:
+				options?.model ||
+				"lmstudio-community/Llama-3-Groq-8B-Tool-Use-GGUF/Llama-3-Groq-8B-Tool-Use-Q5_K_M.gguf",
+			messages: messages,
+			temperature: options?.temperature || 0.7,
+			max_tokens: options?.max_tokens || -1,
+			stream: options?.stream || false,
+			response_format: format,
+		}),
+	}).then(async (r) => JSON.parse((await r.json()).choices[0].message.content));
+};
