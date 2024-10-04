b447386f0947022bcaaea048ba79947ea0468366
Kimball Bradford <kimball.bradford@gmail.com>
2024-10-04 17:54:16 +0200

    switched to basic blocking for css

--- src/pages/Novels/Novels.tsx
+++ src/pages/Novels/Novels.tsx
@@ -41,14 +41,52 @@ export const Novels: FunctionComponent = () => {
 
 	return (
 		<div className={styles.novels}>
-			<header>Chapter Outline</header>
+			<div></div>
+			<div className={styles.outline}>
+				<div className={styles.chapter}>
+					<span>
+						<span className={styles.topMarker}>Down the rabbithole</span>Alice
+						follows a white rabbit down a hole
+					</span>
+					<span>
+						<span>Page Count</span>5
+					</span>
+				</div>
+				<div className={styles.chapter}>
+					<span>
+						<span className={styles.topMarker}>Topsy Turvy Tea</span>Alice has a
+						tea party with the mad hatter
+					</span>
+					<span>
+						<span>Page Count</span>5
+					</span>
+				</div>
+				<div className={styles.chapter}>
+					<span>
+						<span className={styles.topMarker}>Checker Royalty</span>Alice meets
+						the white queen
+					</span>
+					<span>
+						<span>Page Count</span>5
+					</span>
+				</div>
+			</div>
+			<div>
+				Your current word estimate is ~4500 words. A Short Story is normally
+				5000-10000 words long. Don't be worried to break this rule by ~2500
+				words
+			</div>
+		</div>
+	);
+};
+
+{
+	/* <header>Chapter Outline</header>
 			{renderChapters()}
 			<NewChapter
 				createNewChapter={(description, numberOfPages) => {
 					setChapters([...chapters, { description, numberOfPages }]);
 				}}
 			/>
-			<WordEstimator chapters={chapters} />
-		</div>
-	);
-};
+			<WordEstimator chapters={chapters} /> */
+}
